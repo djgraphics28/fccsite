27577451bbfd29bea8007cb1de339c9d04d98acf
djgraphics28 <darwin.ibay@30@gmail.com>
2023-07-26 06:25:16 +0800

    updates

--- resources/views/livewire/members-table.blade.php
+++ resources/views/livewire/members-table.blade.php
@@ -1,56 +1,61 @@
 <div>
-    <div class="row mb-4">
-        <div class="col-md-4">
-          <!-- Search Input -->
-          <input type="text" class="form-control" wire:model="searchTerm" placeholder="Search...">
+    <div class="table-responsive">
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <!-- Search Input -->
+                <input type="text" class="form-control" wire:model="searchTerm" placeholder="Search...">
+            </div>
         </div>
-      </div>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Gender</th>
-                <th>Birth Date</th>
-                <th>Age</th>
-                <th>Contact Number</th>
-                <th>Address</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($records as $row)
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <td>{{ $row->first_name }} {{ $row->middle_name }} {{ $row->last_name }} {{ $row->ext_name }}</td>
-                    <td>{{ $row->gender }}</td>
-                    <td>{{ $row->birth_date }}</td>
-                    <td>{{ $row->birth_date ? date_diff(date_create($row->birth_date), date_create('today'))->y .' years old' : ''; }}</td>
-                    <td>{{ $row->contact_number }}</td>
-                    <td>{{ $row->address }}</td>
-                    <td>
-                        <button class="btn btn-warning">Edit</button>
-                        <button class="btn btn-danger">Remove</button>
-                    </td>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Birth Date</th>
+                    <th>Age</th>
+                    <th>Contact Number</th>
+                    <th>Address</th>
+                    <th>Action</th>
                 </tr>
-            @empty
+            </thead>
+            <tbody>
+                @forelse ($records as $row)
+                    <tr>
+                        <td>{{ $row->first_name }} {{ $row->middle_name }} {{ $row->last_name }} {{ $row->ext_name }}
+                        </td>
+                        <td>{{ $row->gender }}</td>
+                        <td>{{ $row->birth_date }}</td>
+                        <td>{{ $row->birth_date ? date_diff(date_create($row->birth_date), date_create('today'))->y . ' years old' : '' }}
+                        </td>
+                        <td>{{ $row->contact_number }}</td>
+                        <td>{{ $row->address }}</td>
+                        <td>
+                            <button class="btn btn-warning">Edit</button>
+                            <button class="btn btn-danger">Remove</button>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="100%">
+                            <center>No record found</center>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+            <tfoot>
                 <tr>
-                    <td colspan="100%">
-                        <center>No record found</center>
-                    </td>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Birth Date</th>
+                    <th>Age</th>
+                    <th>Contact Number</th>
+                    <th>Address</th>
+                    <th>Action</th>
                 </tr>
-            @endforelse
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Gender</th>
-                <th>Birth Date</th>
-                <th>Age</th>
-                <th>Contact Number</th>
-                <th>Address</th>
-                <th>Action</th>
-            </tr>
-        </tfoot>
-    </table>
+            </tfoot>
+        </table>
+    </div>
+
 
     {{ $records->links() }}
 </div>
