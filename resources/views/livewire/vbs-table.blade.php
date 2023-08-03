0eb2ba0b242e984118a1e03a37cb375adb33e970
djgraphics28 <darwin.ibay29@gmail.com>
2023-08-03 21:46:20 +0800

    updates

--- resources/views/livewire/vbs-table.blade.php
+++ resources/views/livewire/vbs-table.blade.php
@@ -20,6 +20,7 @@
                     <th class="text-center" width="2%"><input type="checkbox" wire:model="selectAll"></th>
                     <th>Name</th>
                     <th>Gender</th>
+                    <th>Age</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -27,8 +28,9 @@
                 @forelse ($records as $row)
                     <tr>
                         <td><input type="checkbox" wire:model.prevent="selectedRows" value="{{ $row->id }}"></td>
-                        <td></td>
-                        <td></td>
+                        <td>{{ $row->member->first_name }} {{ $row->member->last_name }}</td>
+                        <td>{{ $row->member->gender }}</td>
+                        <td>{{ $row->member->age }}</td>
                         <td>
                             {{-- <a wire:click="members" class="btn btn-primary">Members</a>
                             <a href="{{ route('groups.edit', $row->id) }}" class="btn btn-warning">Edit</a>
@@ -48,6 +50,7 @@
                     <th>#</th>
                     <th>Name</th>
                     <th>Gender</th>
+                    <th>Age</th>
                     <th>Action</th>
                 </tr>
             </tfoot>
@@ -57,51 +60,51 @@
     {{ $records->links() }}
 
     <!-- Add Member -->
-    <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel"
-        aria-hidden="true" wire:ignore.self>
-        <div class="modal-dialog modal-xl" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addModalLabel">Kids For Jesus Lists</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th class="text-center" width="2%">#</th>
-                                <th>Name</th>
-                                <th>Age</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($kids as $row)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $row->first_name }} {{ $row->last_name }}</td>
-                                    <td>{{ $row->age }}</td>
-                                    <td><button wire:click="addToVbs({{ $row->id }})"
-                                            class="btn btn-success">ADD</button></td>
-                                </tr>
-                            @empty
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    {{-- @if ($updateMode == true)
+<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true"
+wire:ignore.self>
+<div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="addModalLabel">Kids For Jesus Lists</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th class="text-center" width="2%">#</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($kids as $row)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $row->first_name }} {{ $row->last_name }}</td>
+                            <td>{{ $row->age }}</td>
+                            <td><button wire:click="addToVbs({{ $row->id }})" class="btn btn-success">ADD</button></td>
+                        </tr>
+                    @empty
+
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            {{-- @if ($updateMode == true)
                 <button wire:click.prevent="update()" class="btn btn-success">Update</button>
             @else
                 <button wire:click.prevent="submit()" class="btn btn-primary">Save</button>
             @endif --}}
-                </div>
-            </div>
         </div>
     </div>
+</div>
+</div>
 </div>
 
 
