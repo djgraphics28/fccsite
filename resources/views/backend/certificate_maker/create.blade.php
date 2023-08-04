734634b1308ab068a016ad572f0ed231de1ad7fc
djgraphics28 <darwin.ibay30@gmail.com>
2023-08-04 15:52:44 +0800

    updates

---
+++ resources/views/backend/certificate_maker/create.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.backend')
+
+@section('title', 'Create Certificate Template')
+@push('css')
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+@endpush
+@section('content')
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Create Certificate Template</h1>
+        <a href="{{ route('cert-gen.index') }}" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm"><i
+                class="fas fa-arrow-left fa-md text-white-50"></i> Back</a>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card shadow mb-4">
+                <div class="card-header">
+                    <h4>Create Certificate Template Form</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('cert-gen.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                <label for="title">Title</label>
+                                <input type="text"
+                                    class="form-control form-control-lg @error('title') is-invalid @enderror" id="title"
+                                    name="title">
+
+                                @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-12 mb-3 mb-sm-0">
+                                <label for="summernote">Content</label>
+                                <div id="summernote" name="content"></div>
+                                @error('content')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-sm-12 mb-3 mb-sm-0">
+                                <label for="summernote">Signatories</label>
+                                <div id="inputContainer">
+                                    <!-- Initial input field -->
+                                    <input type="text" name="dynamicInput[]" class="dynamicInput" />
+                                </div>
+                                <button type="button" id="addInput">Add More</button>
+                            </div>
+                        </div>
+
+                        <button class="btn btn-primary mt-4 float-right">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+
+    <script>
+        $('#summernote').summernote({
+            placeholder: 'Hello stand alone ui',
+            tabsize: 2,
+            height: 120,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+        });
+
+        $(document).ready(function () {
+            $('#addInput').click(function () {
+                $('#inputContainer').append('<input type="text" name="dynamicInput[]" class="dynamicInput" />');
+            });
+        });
+    </script>
+@endpush
